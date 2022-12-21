34baab3fd56bee56dc444f629e3dc8005612795d
LeeVaughn <dlvprogramming@gmail.com>
2022-12-21 15:29:02 -0800

    Refactor and improve code

--- app.py
+++ app.py
@@ -1,30 +1,25 @@
+from config import auth_key, endpoint
 import sys
 import time
 import requests
 
-base_endpoint = "https://api.assemblyai.com/v2"
-headers = {'authorization': "your api key here"}
+base_endpoint = endpoint
+headers = {'authorization': auth_key}
 
 
 # Start the transcription process
 def start_transcript(audio_url):
     post_json = {
         "audio_url": audio_url,
+        # "dual_channel": True,
         # "language_code": "it",
         # "language_detection": True,
         # "punctuate": False,
         # "format_text": False,
-        "word_boost": ["covid"],
-        "boost_param": "high",
-        # "custom_spelling": [{"from": ["ariana"], "to": "Arianna"}], 
-        # "dual_channel": True,
-        # "entity_detection": True,
-        # "auto_highlights": True,
-        # "iab_categories": True,
-        # "content_safety": True,
-        # "sentiment_analysis": True,
-        # "auto_chapters": True,
         # "speaker_labels": True,
+        # "word_boost": ["covid"],
+        # "boost_param": "high",
+        # "custom_spelling": [{"from": ["ariana"], "to": "Arianna"}], 
         # "disfluencies": True,
         # "filter_profanity": True,
         # "redact_pii": True,
@@ -32,11 +27,18 @@ def start_transcript(audio_url):
         # "redact_pii_policies": [
         #     "medical_process", "medical_condition", "blood_type", "drug", "injury", "number_sequence", "email_address", "date_of_birth", "phone_number", "us_social_security_number", "credit_card_number", "credit_card_expiration", "credit_card_cvv", "date", "nationality", "event", "language", "location", "money_amount", "person_name", "person_age", "organization", "political_affiliation", "occupation", "religion", "drivers_license", "banking_information"
         # ],
-        "summarization": True,
-        "summary_type": "bullets"
+        # "auto_highlights": True,
+        # "content_safety": True,
+        # "iab_categories": True,
+        # "sentiment_analysis": True,
+        # "summarization": True,
+        # "summary_model": "informative",
+        # "summary_type": "bullets"
+        # "entity_detection": True,
+        # "auto_chapters": True,
     }
 
-    r = requests.post(base_endpoint + "/transcript", headers=headers, json=post_json)
+    r = requests.post(base_endpoint + '/transcript', headers=headers, json=post_json)
 
     if 'error' in r.json():
         print(r.json())
@@ -46,24 +48,23 @@ def start_transcript(audio_url):
 
 # Get the completed transcription
 def get_transcript(id):
-    r = requests.get(base_endpoint + "/transcript/" + id, headers=headers)
+    r = requests.get(base_endpoint + '/transcript/' + id, headers=headers)
     return r.json()
 
 
 # Wait for the status of the transcription to be completed
 def wait_for_result(id):
-    # print("polling for result...")
     response = get_transcript(id)
     while response['status'] not in ['completed', 'error']:
         time.sleep(3)
         response = get_transcript(response['id'])
-        # print(response['status'])
     return response
 
 
 def main(audio_url):
+    print('Submitting file: ', audio_url)
     response = start_transcript(audio_url)
-    print("transcript id: %s" % response['id'])
+    print('transcript id: %s' % response['id'])
     response = wait_for_result(response['id'])
 
     if response['status'] == 'error':
@@ -73,7 +74,7 @@ def main(audio_url):
     return response
  
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     audio_url = sys.argv[1]
     print(audio_url)
     main(audio_url)
--- batch.py
+++ batch.py
@@ -1,7 +1,6 @@
 import json
 import os
 from concurrent.futures import ThreadPoolExecutor
-
 from app import main
 
 lines = []
@@ -9,52 +8,27 @@ lines = []
 
 def run_file(audio_url, file_name):
     data = main(audio_url)
-    text_filename = file_name + ".txt"
-    json_filename = file_name + ".json"
+    text_filename = file_name + '.txt'
+    json_filename = file_name + '.json'
     
-    text_file = open("./text/" + text_filename, 'x', encoding='utf8')
-    json_file = open("./json/" + json_filename, 'x', encoding='utf8')
+    text_file = open('./text/' + text_filename, 'x', encoding='utf8')
+    json_file = open('./json/' + json_filename, 'x', encoding='utf8')
 
     text_file.write(data['text'])
     text_file.close()
     json_file.write(json.dumps(data, ensure_ascii=False, indent=2))
     json_file.close()
 
-    for word in data['words']:
-        if word['end'] <= word['start']:
-            print(file_name)
-            ts_filename = file_name + ".json"
-            ts_file = open("./exceptions/" + ts_filename, 'x')
-
-            ts_file.write(json.dumps(data, indent=2))
-            ts_file.close()
-
-    # # creates a text file of turn by turn utterances
-    # if (len(data.get('utterances'))) >= 1:
-    #     utterances = data['utterances']
-    #     text = ''
-    #     file_name = file_name + ".txt"
-    #     # print(utterances)
-
-    #     for utterance in utterances:
-    #         text += f"Speaker {utterance['speaker']}: {utterance['text']}\n\n"
-    #         # print(utterance['speaker'])
-    #     diarization_filename = file_name + ".txt"
-    #     diarization_file = open("./diarization/" + diarization_filename, 'x')
-
-    #     diarization_file.write(text)
-    #     diarization_file.close()
-
 
 # uploads files to get back an upload URL
 print('Uploading files...')
 os.system('upload.py')
 
-# deletes any files in the json, text, or diarization directories
+# deletes any files in the json or text directories
 print('Emptying folders...')
 os.system('empty_folders.py')
 
-f = open("urls.txt", "r")
+f = open('urls.txt', 'r')
 
 # loop over each line in text file and append them to dictionary
 for x in f:
@@ -63,6 +37,6 @@ for x in f:
 with ThreadPoolExecutor(10) as executor:
     for line in lines:
         # splits file names and urls
-        split_line = line.split(",")
-        split_url = split_line[1].split("\n")
+        split_line = line.split(',')
+        split_url = split_line[1].split('\n')
         executor.submit(run_file, split_url[0], split_line[0])
